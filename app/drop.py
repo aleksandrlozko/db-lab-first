360d3096486db3003e411ebb67d71fe4888e367d
aleksandrlozko <44771725+aleksandrlozko@users.noreply.github.com>
2021-03-09 01:52:46 +0200

    Update drop.py

--- app/drop.py
+++ app/drop.py
@@ -4,7 +4,7 @@ from app.config import config
 conn = psycopg2.connect(**config)
 cur = conn.cursor()
 cur.execute("DROP TABLE table_result")
-cur.execute("DROP TABLE table_result")
+cur.execute("DROP TABLE table_temp")
 cur.close()
 conn.commit()
 conn.close()
